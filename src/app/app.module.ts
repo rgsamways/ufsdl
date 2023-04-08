d84760a36f250d06e58e4fdc35644bce7c8df4bf
Robin <30755265+rgsamways@users.noreply.github.com>
2023-04-08 17:44:19 -0400

    First try using Github REST API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterbarComponent } from './footerbar/footerbar.component';
+import { MainbarComponent } from './mainbar/mainbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterbarComponent,
+    MainbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
